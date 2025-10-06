4935bfd282ed5ac254c4edd1662095764b89cdcf
fcasmar2101 <fcasmar2101@ieszaidinvergeles.org>
2025-10-06 12:32:54 +0200

    Ejercicio 8

---
+++ 8.py
@@ -0,0 +1,7 @@
+numero1 = float(input("Introduce un número: "))
+numero2 = float(input("Introduce otro número: "))
+
+cociente = numero1 / numero2
+resto = numero1 % numero2
+
+print(f"Los números introducidos por el usuario son: {numero1} y {numero2}, y el cociente y resto respectivamente son: {cociente} y {resto}")
