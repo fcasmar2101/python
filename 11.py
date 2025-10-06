98788398e55563245d45807176c54ca64791f201
fcasmar2101 <fcasmar2101@ieszaidinvergeles.org>
2025-10-06 12:48:17 +0200

    Ejercicio 11 y correccion de nuevo del 7

---
+++ 11.py
@@ -0,0 +1,7 @@
+ahorros=float(input("Cantidad inicial "))
+ahorros1=(ahorros*4/100)+ahorros
+ahorros2=(ahorros1*4/100)+ahorros1
+ahorros3=(ahorros2*4/100)+ahorros2
+print(ahorros1)
+print(ahorros2)
+print(ahorros3)
--- 7.py
+++ 7.py
@@ -1,4 +1,4 @@
 peso=float(input("Introduce tu peso en KG: "))
 altura=float(input("Introduce tu peso en CM: "))
 imc=int(peso)/(altura)**2
-print(f"Tu IMC es: {imc:.2f}")
+print(f"Tu IMC es: ", round(imc,2))
