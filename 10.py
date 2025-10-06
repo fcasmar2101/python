d4bba2e1b2936a8de212f9e54a45bef4e1017b7e
fcasmar2101 <fcasmar2101@ieszaidinvergeles.org>
2025-10-06 12:41:09 +0200

    Ejercicio 10 y correccion del 7

---
+++ 10.py
@@ -0,0 +1,6 @@
+payaso=float(input("Numero de payasos "))
+muñeca=float(input("Numero de muñecas "))
+Ppayaso=payaso*112
+Pmuñeca=muñeca*75
+Ptotal=Ppayaso+Pmuñeca
+print(f"{Ptotal} Gramos")
--- 7.py
+++ 7.py
@@ -1,4 +1,4 @@
 peso=float(input("Introduce tu peso en KG: "))
 altura=float(input("Introduce tu peso en CM: "))
 imc=int(peso)/(altura)**2
-print (imc)
+print(f"Tu IMC es: {imc:.2f}")
