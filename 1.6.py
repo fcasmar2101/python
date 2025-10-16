320a88b1e7c275887fc810b1591827c1299ea736
fcasmar2101 <fcasmar2101@ieszaidinvergeles.org>
2025-10-16 10:00:10 +0200

    Ejercicio 3.2

--- 1.6.py
+++ 1.6.py
@@ -1,3 +1,3 @@
 numero=int(input("Dime un numero entero "))
 suma=(numero*(numero+1))/2
-print("La suma de los enteros de 1 hasta", numero, "es:", suma)
+print(f"La suma de los enteros de 1 hasta {numero} es {suma}")
