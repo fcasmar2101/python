85e0d27aa4f415430d27cdcda84753768c9f13c3
fcasmar2101 <fcasmar2101@ieszaidinvergeles.org>
2025-10-16 10:21:16 +0200

    Ejercicio 3.5

---
+++ 3.5.py
@@ -0,0 +1,6 @@
+edad=int(input("Dime tu edad "))
+ingresos=int(input("Dime tus ingresos "))
+if(edad>=16 and ingresos>=1000):
+    print("Tienes que tributar")
+else:
+    print("No tienes que tributar")
