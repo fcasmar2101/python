689d6315efb286e9770443192bf44ad3cdf375c0
fcasmar2101 <fcasmar2101@ieszaidinvergeles.org>
2025-10-30 10:39:22 +0100

    Ejercicio 4.5

---
+++ 4.5.py
@@ -0,0 +1,8 @@
+cantidad=float(input("Cantidad a invertir "))
+interes=int(input("Interes anual "))
+años=int(input("Numero de años "))
+intereses=0
+for i in range(años):
+    intereses=(cantidad*interes)/100
+    cantidad+=intereses
+    print (f"Tu capital el años {i} sera de {cantidad}")
